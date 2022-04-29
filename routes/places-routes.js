afd183b8cb162ca1cd1c81728a306411437c7488
axel77 <axel@bearstudio.fr>
2022-04-29 17:53:27 +0200

    Add custom middleware for auth

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const placesController = require("../controllers/places-controller");
 const { check } = require("express-validator");
+const checkAuth = require("../middleware/check-auth");
 
 // write a new route able to accept a dynamic uid segment,
 // extract it in the middleware function and find a place where the creator has that user ID.
 router.get("/", placesController.getPlaces);
 router.get("/:pid", placesController.getPlaceById);
 router.get("/users/:uid", placesController.getPlacesByUserId);
+
+router.use(checkAuth);
+
 router.post(
   "/",
   [
@@ -18,11 +22,11 @@ router.post(
   placesController.createPlace
 );
 
-router.patch("/:pid",
-[
-  check("title").not().isEmpty(),
-  check("description").isLength({ min: 5 }),
-], placesController.updatePlace);
+router.patch(
+  "/:pid",
+  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+  placesController.updatePlace
+);
 
 router.delete("/:pid", placesController.deletePlace);
 
